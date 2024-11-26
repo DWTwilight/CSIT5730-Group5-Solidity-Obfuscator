c56f994604720f17cd84c0c4b7c1d12807e0e88d
JiangYH <352826502@qq.com>
2024-11-26 20:36:40 +0800

    update sample

--- examples/example1.sol
+++ examples/example1.sol
@@ -2,6 +2,15 @@
 pragma solidity ^0.8.26;
 
 contract Loop {
+    uint256 constant l1 = 10;
+    uint256 constant l2 = 20;
+    uint256 constant l3 = l1 / l2;
+    uint256 constant p = l1 * l2 % 20 + 3 ** l3;
+    bool constant abcd = true;
+    uint256[p] public value_1;
+    uint256[(((l1)+l2))] public value_2;
+    uint256[p][l1][l1+l2+p-p+1] public multi_array;
+    uint256[10] public value_3 = [1,2,3,4,5,6,7,8,9,10];
     function loop() public pure returns (uint256, uint256) {
         // for loop
         uint256 s;
@@ -32,5 +41,36 @@ contract Loop {
             j += 9;
         }
         return (s, j);
+    }
+    function set_value(uint256 _value, uint128 idx) public {
+        if(idx < value_1.length - 1){
+            value_1[idx] = _value + p + value_3[4];
+        }
+        dynamic_value.push(_value);
+        
+    }
+
+    function get_value(uint128 idx) public view returns (uint256){
+        if(value_3.length > 5)
+            return value_1[idx] * l3;
+        else
+            return value_1[idx];
+    }
+
+    function replace_value(uint[10] memory data, uint idx) public returns (uint256[10] memory){
+        value_1[idx] = data[idx];
+        data[idx-1] = value_1[idx-1];
+        return data;
+    }
+
+    function add_value(uint[] memory data, uint idx) public view returns (uint[11] memory){
+        uint[11] memory new_data;
+
+        for(uint i=0; (i< new_data.length-1) && (i < data.length); i++)
+        {
+            new_data[i] = data[i];
+        }
+        new_data[new_data.length-1] = value_1[idx];
+        return new_data;
     }
 }
