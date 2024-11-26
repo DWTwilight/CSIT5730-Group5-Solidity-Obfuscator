59fd686d4a2965304c20a4c5cd3cf0aec9df38d5
Tianci Zhao <446416074@qq.com>
2024-11-26 21:24:19 +0800

    add e2

---
+++ examples/example2.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract ControlFlowExample {
+    function loop() public pure returns (uint256, uint256) {
+        // for loop
+        uint256 s;
+        for (uint256 i = 0; i < 10; i++) {
+            if (i == 3) {
+                // Skip to next iteration with continue
+                s += i;
+                continue;
+            }
+            if (i == 5) {
+                // Exit loop with break
+                s -= i;
+                break;
+            }
+        }
+
+        if (s > 5) {
+            s -= 5;
+        }
+
+        // while loop
+        uint256 j;
+        while (j <= 10) {
+            j++;
+        }
+
+        if (j >= 20) {
+            j += 9;
+        }
+        return (s, j);
+    }
+}
