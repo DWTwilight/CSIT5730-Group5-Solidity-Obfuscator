997458b7414b12c4f82821ee6bd3433f7549678c
Tianci Zhao <446416074@qq.com>
2024-11-27 22:40:21 +0800

    add benchmark project

---
+++ gas-consumption-benchmark/test/Example2.test.js
@@ -0,0 +1,24 @@
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Example2 Test", function () {
+  async function deployContract() {
+    const ControlFlowExample = await ethers.getContractFactory(
+      "ControlFlowExample"
+    );
+    const target = await ControlFlowExample.deploy();
+    const users = await ethers.getSigners();
+
+    return { target, users };
+  }
+
+  it("test1", async function () {
+    const { target, users } = await loadFixture(deployContract);
+
+    await target.loop();
+    // expect(await target.loop()).to.be.revertedWith("aaa");
+  });
+});
