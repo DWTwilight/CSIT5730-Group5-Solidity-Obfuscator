80f030c394402db8a34056a28528be1e91ffae80
Tianci Zhao <446416074@qq.com>
2024-12-09 17:34:38 +0800

    ignore buggy part

--- layout_and_data_obfuscation/combination.py
+++ layout_and_data_obfuscation/combination.py
@@ -12,6 +12,7 @@ output_file_path = sys.argv[2]
 
 deleteComment.clean_and_save(input_file_path, output_file_path)
 variableTransformar.variableTransformar(output_file_path, output_file_path)
-codeRearrage.shuffle_and_indent_methods(output_file_path, output_file_path)
+# TODO: fix bug in this part
+# codeRearrage.shuffle_and_indent_methods(output_file_path, output_file_path)
 
 # disruptFormat.process_file(output_file_path, output_file_path, 0.5)
